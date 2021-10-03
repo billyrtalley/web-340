b59a986cae7e846bcde2fe7e09d6809a75d49030
William Talley <wtalley@my365.bellevue.edu>
2021-10-03 14:58:25 -0500

    Ex 8.3 csrf protection

--- week-5/ems/app.js
+++ week-5/ems/app.js
@@ -16,26 +16,39 @@
 ; Modified By: William Talley
 ; Description: Javascript to EMS; modified the original app.js file to
 ; include the mongoose connection code and Employee model.
-
+;
 ; Title: Exercise 8.2 Cross Site Scripting
 ; Author: Professor Krasso 
 ; Date Modified: 3 October 2021
 ; Modified By: William Talley
 ; Description: Exercise is designed to practice installing helmet to our ems application
 ; to protect against cross-site scripting
+;
+; Title: Exercise 8.3 Cross Site Request Forgery
+; Author: Professor Krasso 
+; Date Modified: 3 October 2021
+; Modified By: William Talley
+; Description: Exercise is designed to practice protecting against
+; cross site request forgery. The objective is to create a random CSRF token every time the
+; user is asked for data and to validate the random token every time i deal with the data
 ;===========================================
 */ 
 
 //declaration: require statements for libraries: express,  morgan, http object, and the mongoose connection and models folder
 //week 8 add require statement for helmet
+//8.3 add body-parser, cookie-parser, and csurf to require statements
 var express = require("express");
 var http = require("http");
 var path = require("path");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 var helmet = require("helmet");
+var bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
+var csrf = require("csurf");
 var Employee = require("./models/employee");
 const { once } = require("events");
+const { urlencoded } = require("body-parser");
 
 //set up the connection string
 var mongoDB = 'mongodb+srv://billyrtalley:TRIumph2014@buwebdev-cluster-1.wmilj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
@@ -56,6 +69,9 @@ db.once ("open", function() {
     console.log("Application connection to MongoDB Atlas instance");
 });
 
+//week 8 (8.3) add variable to setup csrf protection
+var csrfProtection = csrf({cookie: true});
+
 // assignment: variable app assigned to express
 var app = express();
 
@@ -70,7 +86,21 @@ app.use(logger('short'));
 // week 8- add a use statement for helmet
 app.use(helmet.xssFilter());
 
+//week 8 (8.3) use statements for body-parser, cookie-parser and csurf
+app.use (
+    bodyParser, urlencoded({
+        extended: true,
+    })  
+);
 
+app.use(cookieParser());
+app.use(csrfProtection);
+app.use(function (request, response, next) { 
+    var token = request.csrfToken();
+    response.cookie("XSRF-Token", token);
+    response.locals.csrfToken = token;
+    next();
+});
 
 
 //add variable for employee
@@ -86,6 +116,20 @@ app.get("/", function (request, response) {
         title: "Home page",
         message: "XSS prevention example"
     });
+});
+
+//add a new message for 8.3
+app.get("/new", function (request, response) { 
+    response.render("new", {
+        message: "message added for Exercise 8.3",
+    });
+});
+
+//week 8(8.3) add new routing for exercise 8.3
+
+app.post("/process", function (request, response) { 
+    console.log(request.body.txtName);
+    response.redirect("/");
 });
 
 //set folder for the css style
